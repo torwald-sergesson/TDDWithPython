68a819b006b2cc101cee05fe1eb21bfc8d68c16f
Vladimir Kasatkin <kasatkin@saritasa.com>
2016-07-28 13:34:59 +0700

    Updated FT to check second item input

--- functional_tests.py
+++ functional_tests.py
@@ -7,7 +7,6 @@ BROWSER_WAIT_TIME = 3  # seconds
 
 
 class NewVisitorTest(unittest.TestCase):
-
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(BROWSER_WAIT_TIME)
@@ -16,7 +15,6 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrive_it_later(self):
-
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
 
@@ -44,25 +42,32 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in the table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                      [row.text for row in rows])
+
 
         # Edith wonders whether the site will remembers her list.
         # then she sees that site has generated a unique URL for her --
         # there is some explanatory text to that effect.
+        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there
 
         # Satisfied, she goes back to sleep
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
